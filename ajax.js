57ead687d0fdca9fff38d0047ebf49d1d384b4e2
Duyvu98 <Duyvu98>
2019-12-23 15:21:04 +0700

    bn

--- ajax.js
+++ ajax.js
@@ -16,27 +16,8 @@ var allowCrossDomain = function(req, res, next) {
 }
 server.use(allowCrossDomain)
 
-server.post('/', function(req,res,next){
-    res.json(req.body)
-})
-
-
-server.get('/login',function(req, res, next){
-    res.json('Abc')
-})
-
-var isLogin = false;
-
-server.post('/login', function(req,res,next){
-    if(req.body.username === 'admin'&& req.body.password==='1234' ){
-        "Chúc Mừng Đã Đăng Nhập Thành Công"
-    } else{
-        res.redirect('/login')
-    }
-})
-
-server.get('/', function(req, res, next){
-    res.sendFile(path.join(__dirname,'index.html'))
+server.get('/', function(req,res,next){
+    res.json('888888888888888888888')
 })
 server.listen(process.env.PORT|| 3000,function(){
     console.log('listen on 3000')
